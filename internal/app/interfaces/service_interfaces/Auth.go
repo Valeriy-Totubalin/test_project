10327fd5e25a0f395db080a20b912a22fcdf08cf
Valera <totubalinvaleriy@mail.ru>
2021-03-18 20:36:59 +0300

    create interface Auth Service

---
+++ internal/app/interfaces/service_interfaces/Auth.go
@@ -0,0 +1,9 @@
+package service_interfaces
+
+import "github.com/Valeriy-Totubalin/test_project/internal/domain"
+
+type AuthService interface {
+	SignUp(user domain.User) error
+	SignIn(secret string, user domain.User) (string, error) // return token
+	LogOut(userId int) error
+}
