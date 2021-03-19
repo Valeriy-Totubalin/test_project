e12c4786c26382a2869c143d06a696ca50ddb52f
Valera <totubalinvaleriy@mail.ru>
2021-03-19 22:48:14 +0300

    refactoring item repository interface

--- internal/app/interfaces/repository_interfaces/ItemRepository.go
+++ internal/app/interfaces/repository_interfaces/ItemRepository.go
@@ -4,6 +4,6 @@ import "github.com/Valeriy-Totubalin/test_project/internal/domain"
 
 type ItemRepository interface {
 	Create(item *domain.Item) error
-	Delete(item *domain.Item) error
+	DeleteById(itemId int) error
 	GetAll() ([]*domain.Item, error)
 }
