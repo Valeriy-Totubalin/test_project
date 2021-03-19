3231f0de768aec828e076856bf39e436a86a839d
Valera <totubalinvaleriy@mail.ru>
2021-03-19 20:37:51 +0300

    method log in

--- internal/service/AuthService_test.go
+++ internal/service/AuthService_test.go
@@ -2,11 +2,23 @@ package service
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Valeriy-Totubalin/test_project/internal/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
+
+type TokenManagerMock struct {
+	mock.Mock
+}
+
+func (m *TokenManagerMock) NewJWT(userId int, ttl time.Duration) (string, error) {
+	m.Called(userId, ttl)
+
+	return "token", nil
+}
+func (m *TokenManagerMock) Parse(accessToken string) (int, error) { return 0, nil }
 
 type PasswordHasherMock struct {
 	mock.Mock
@@ -18,29 +30,44 @@ func (m *PasswordHasherMock) GenerateHash(password string) (string, error) {
 	return "password_hash", nil
 }
 
-func (m *PasswordHasherMock) CheckPassword(password string, hashedPassword string) error { return nil }
+func (m *PasswordHasherMock) CheckPassword(password string, hashedPassword string) error {
+	m.Called(password, hashedPassword)
+
+	return nil
+}
 
 type UserRepositoryMock struct {
 	mock.Mock
 }
 
-func (m *UserRepositoryMock) Create(user domain.User) error {
+func (m *UserRepositoryMock) Create(user *domain.User) error {
 	m.Called(user)
 
 	return nil
 }
 
-func (m *UserRepositoryMock) GetByLogin(login string) (*domain.User, error) { return nil, nil }
+func (m *UserRepositoryMock) GetByLogin(login string) (*domain.User, error) {
+	m.Called(login)
+
+	return &domain.User{
+		Id:       1,
+		Login:    "test_login",
+		Password: "password_hash",
+	}, nil
+}
 
 func TestNewAuthService(t *testing.T) {
 	repository := new(UserRepositoryMock)
 	passwordHasher := new(PasswordHasherMock)
+	tokenManager := new(TokenManagerMock)
 
 	serviceExpected := &AuthService{
 		PasswordHasher: passwordHasher,
 		UserRepository: repository,
+		TokenManager:   tokenManager,
 	}
-	serviceEqual := NewAuthService(repository, passwordHasher)
+
+	serviceEqual := NewAuthService(repository, passwordHasher, tokenManager)
 
 	assert.Equal(t, serviceExpected, serviceEqual)
 }
@@ -48,13 +75,14 @@ func TestNewAuthService(t *testing.T) {
 func TestSignUp(t *testing.T) {
 	passwordHasher := new(PasswordHasherMock)
 	repository := new(UserRepositoryMock)
-	service := NewAuthService(repository, passwordHasher)
+	tokenManager := new(TokenManagerMock)
+	service := NewAuthService(repository, passwordHasher, tokenManager)
 
-	user := domain.User{
+	user := &domain.User{
 		Login:    "test_login",
 		Password: "test_password",
 	}
-	userWithHashedPassword := domain.User{
+	userWithHashedPassword := &domain.User{
 		Login:    "test_login",
 		Password: "password_hash",
 	}
@@ -66,6 +94,39 @@ func TestSignUp(t *testing.T) {
 
 	passwordHasher.AssertExpectations(t)
 	repository.AssertExpectations(t)
+
+	assert.Equal(t, nil, err)
+}
+
+func TestSignIn(t *testing.T) {
+	passwordHasher := new(PasswordHasherMock)
+	repository := new(UserRepositoryMock)
+	tokenManager := new(TokenManagerMock)
+	service := NewAuthService(repository, passwordHasher, tokenManager)
+	token := "token"
+
+	user := &domain.User{
+		Id:       1,
+		Login:    "test_login",
+		Password: "test_password",
+	}
+
+	userReturned := &domain.User{
+		Id:       1,
+		Login:    "test_login",
+		Password: "password_hash",
+	}
+
+	repository.On("GetByLogin", user.Login).Return(userReturned).Once()
+	passwordHasher.On("CheckPassword", user.Password, userReturned.Password).Once()
+	tokenManager.On("NewJWT", user.Id, 15*time.Minute).Return(token).Once()
+
+	tokenResult, err := service.SignIn(user)
+	assert.Equal(t, token, tokenResult)
+
+	passwordHasher.AssertExpectations(t)
+	repository.AssertExpectations(t)
+	tokenManager.AssertExpectations(t)
 
 	assert.Equal(t, nil, err)
 }
